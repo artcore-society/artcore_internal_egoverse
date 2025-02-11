9bee1a33069dbf39680aa127e3d276295723a8e3
Dennis Ego <dennis.ego@artcoresociety.com>
2025-02-11 15:05:37 +0100

    Make animations work for visitors

--- app/src/js/Classes/ExperienceManager.ts
+++ app/src/js/Classes/ExperienceManager.ts
@@ -81,6 +81,10 @@ export default class ExperienceManager {
 		ExperienceSocket.on(SocketEvent.CLIENT_UPDATE_PLAYER, (data: {id: string, delta: number, keysPressed: { [key in KeyboardKey]: boolean }}) => {
 			// Update avatar
 			if (this.activeScene && this.activeScene.visitorAvatars) {
+				// Update the mixer of the visitor avatar
+				this.activeScene.visitorAvatars[data.id]?.mixer?.update(data.delta);
+
+				// Update the controls of the visitor avatar
 				this.activeScene.visitorAvatars[data.id]?.controls?.update(data.delta, data.keysPressed);
 			}
 		});
