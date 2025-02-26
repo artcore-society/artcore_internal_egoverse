52b3c0be2558a24acbfeb5d0292ffd6d9a2382a6
Dennis Ego <dennis.ego@artcoresociety.com>
2025-02-26 08:11:16 +0100

    Add elvis avatar

--- app/src/ts/Stores/AvatarStore.ts
+++ app/src/ts/Stores/AvatarStore.ts
@@ -8,7 +8,8 @@ export const useAvatarStore = defineStore('avatarStore', () => {
 	const avatars: Ref<Array<{ id: number; asset: string; name: string }>> = ref([
 		{ id: 1, asset: '/assets/images/avatars/1.jpg', name: 'Worker' },
 		{ id: 2, asset: '/assets/images/avatars/2.jpg', name: 'Knight' },
-		{ id: 3, asset: '/assets/images/avatars/3.jpg', name: 'Mouse' }
+		{ id: 3, asset: '/assets/images/avatars/3.jpg', name: 'Mouse' },
+		{ id: 4, asset: '/assets/images/avatars/4.jpg', name: 'Elvis' },
 	]);
 
 	// Methods
