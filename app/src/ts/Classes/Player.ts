7ddec4030c2a8b3bc462402fa3edda1784086b6d
Dennis Ego <dennis.ego@artcoresociety.com>
2025-03-04 15:03:50 +0100

    Test commite

--- app/src/ts/Classes/Player.ts
+++ app/src/ts/Classes/Player.ts
@@ -31,6 +31,7 @@ export default class Player implements IPlayer {
 		spawnPosition: Vector3 = new Vector3(),
 		spawnRotation: Quaternion = new Quaternion()
 	) {
+		console.log('test');
 		this.username = username;
 		this.modelId = modelId;
 		this.experienceScene = experienceScene;
