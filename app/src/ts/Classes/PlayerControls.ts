b419e02ed0ce600386d611177b1e0355374ba408
Dennis Ego <dennis.ego@artcoresociety.com>
2025-03-05 10:05:29 +0100

    Make sure current player camera parent position is reset to spawn position

--- app/src/ts/Classes/PlayerControls.ts
+++ app/src/ts/Classes/PlayerControls.ts
@@ -4,5 +4,14 @@ import BaseCharacterControls from './BaseCharacterControls.ts';
 export default class PlayerControls extends BaseCharacterControls {
 	constructor(player: Player) {
 		super(player);
+
+		if (player.isCurrent) {
+			// Make sure camera parent position is starts at spawn position
+			player.experienceScene.cameraParent.position.set(
+				player.spawnPosition.x,
+				player.spawnPosition.y,
+				player.spawnPosition.z
+			);
+		}
 	}
 }
