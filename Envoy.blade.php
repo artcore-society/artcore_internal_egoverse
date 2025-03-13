e67ae1da810eeb4cf8f5b4a12be540e64332edc0
Dennis Ego <dennis.ego@artcoresociety.com>
2025-03-13 11:02:54 +0100

    Add pm2 to server dependancies

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -6,10 +6,12 @@ git fetch --prune
 git stash
 git pull origin main
 
+# Build the frontend app
 cd ./app
 yarn
 yarn build
 
+# Install dependencies and restart the socket server
 cd ../server
 yarn
 npx pm2 restart ./workers/staging-egoverse-socket-server.yaml
